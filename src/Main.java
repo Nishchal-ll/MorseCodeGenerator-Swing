faf07f3677c92f8c94877d2243a65b0c9096b9e8
Nishchal <acharyanischal2004@gmail.com>
2025-10-30 21:50:54 +0545

    Frame COnstruction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setTitle("Morse Code Translator");
+        frame.setLocation(80,50);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
     }
 }
