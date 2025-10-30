c1b75cce5cdfb655d72604db4b6e174b81266754
Nishchal <acharyanischal2004@gmail.com>
2025-10-30 22:23:32 +0545

    Set Icon and Bg

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(800, 600);
         frame.setVisible(true);
         frame.setTitle("Morse Code Translator");
-        frame.setLocation(80,50);
+        frame.setLocation(550,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
+
+       ImageIcon icon = new ImageIcon("icon.png");
+       frame.setIconImage(icon.getImage());
+       frame.getContentPane().setBackground(Color.black);
 
     }
 }
