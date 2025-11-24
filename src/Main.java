23f0407bfc2a2da937b47c788ca50e81d41508c1
Nishchal-ll <acharyanischal2004@gmail.com>
2025-11-24 22:13:00 +0545

    Added translator logic

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,18 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class Main {
-    private static final HashMap<Character, String> morseMap = new HashMap<>();
-
-    static {
-        morseMap.put('A', ".-");
-        morseMap.put('B', "-...");
-        morseMap.put('C', "-.-.");
-        morseMap.put('D', "-..");
-        morseMap.put('E', ".");
-        morseMap.put('F', "..-.");
-        morseMap.put('G', "--.");
-        morseMap.put('H', "....");
-        morseMap.put('I', "..");
-        morseMap.put('J', ".---");
-        morseMap.put('K', "-.-");
-        morseMap.put('L', ".-..");
-        morseMap.put('M', "--");
-        morseMap.put('N', "-.");
-        morseMap.put('O', "---");
-        morseMap.put('P', ".--.");
-        morseMap.put('Q', "--.-");
-        morseMap.put('R', ".-.");
-        morseMap.put('S', "...");
-        morseMap.put('T', "-");
-        morseMap.put('U', "..-");
-        morseMap.put('V', "...-");
-        morseMap.put('W', ".--");
-        morseMap.put('X', "-..-");
-        morseMap.put('Y', "-.--");
-        morseMap.put('Z', "--..");
-        morseMap.put('1', ".----");
-        morseMap.put('2', "..---");
-        morseMap.put('3', "...--");
-        morseMap.put('4', "....-");
-        morseMap.put('5', ".....");
-        morseMap.put('6', "-....");
-        morseMap.put('7', "--...");
-        morseMap.put('8', "---..");
-        morseMap.put('9', "----.");
-        morseMap.put('0', "-----");
-        morseMap.put(' ', " ");
-    }
-
-    public static String toMorseCode(String text) {
-        StringBuilder morse = new StringBuilder();
-        for (char c : text.toUpperCase().toCharArray()) {
-            String code = morseMap.get(c);
-            if (code != null) {
-                morse.append(code).append(" ");
-            }
-        }
-        return morse.toString();
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        JFrame f = new JFrame("Morse Code Generator");
+        f.setVisible(true);
+        f.setSize(500,500);
+        f.setLayout(new FlowLayout());
 
-        System.out.println("Enter text to convert to Morse code:");
-        String input = scanner.nextLine();
-
-        String morseCode = toMorseCode(input);
-        System.out.println("Morse Code:");
-        System.out.println(morseCode);
-
-        scanner.close();
+        JLabel j1 = new JLabel("Enter the text");
+        f.add(j1);
+        JTextField t1 = new JTextField();
+        f.add(t1);
+        JButton  b1 = new JButton("Generate");
+        f.add(b1);
     }
 }
